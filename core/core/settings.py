054bc74f8cddcaeaf73089a486d3a9d2e711d113
osman-abi <osman.97.abi@gmail.com>
2022-08-01 02:59:44 +0400

    settings file changed

--- core/core/settings.py
+++ core/core/settings.py
@@ -197,8 +197,8 @@ CORS_ALLOW_METHODS = [
 
 # celery configuration
 
-CELERY_BROKER_URL = 'redis://3.70.24.182:6379'
-CELERY_RESULT_BACKEND = 'redis://3.70.24.182:6379'
+CELERY_BROKER_URL = 'redis://localhost:6379'
+CELERY_RESULT_BACKEND = 'redis://localhost:6379'
 CELERY_ACCEPT_CONTENT = ['application/json']
 CELERY_TASK_SERIALIZER = 'json'
 CELERY_RESULT_SERIALIZER = 'json'
