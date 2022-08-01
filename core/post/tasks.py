73a97bda93321183abfd8ad925863c742f4c3b30
osman-abi <osman.97.abi@gmail.com>
2022-08-01 09:51:42 +0400

    settings file changed

--- core/post/tasks.py
+++ core/post/tasks.py
@@ -6,8 +6,5 @@ from .models import Post
 @shared_task
 def reset_post_vote():
     """We're filtering all posts which vote count greater than 0"""
-    posts = Post.objects.filter(vote_count__gt=0)
-    for post in posts:
-        post.vote_count = 0
-        post.save()
+    Post.objects.filter(vote_count__gt=0).update(vote_count=0)
     return {"response": "Task was successfully finished"}
